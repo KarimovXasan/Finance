1b6a538d70de0de75f8932c25fbb9cfbc71db24e
XasanboyKarimov <xkarimov257@gmail.com>
2024-10-30 15:11:07 +0500

    Full figma images config

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth:{
+        '1140':'1140px'
+      },
+      colors:{
+        "darkBlue":"#111849",
+        "blue":"#3029D9",
+        "gray":"#A0ABB8",
+        "liteBlue":"#5975FF",
+        "bgColor":"#D6D4F7"
+      },
+      boxShadow:{
+        "startShadow":"0px 50px 100px 0px rgba(0, 0, 0, 0.1)",
+        "service":"0px 10px 100px 10px rgba(0, 0, 0, 0.1)"
+      },
+
+    },
+  },
+  plugins: [],
+}
